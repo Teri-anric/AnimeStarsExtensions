b9f88c1a99f7e43e73069ca5912f50a0402f13cc
Teri-anric <2005ahi2005@gmail.com>
2026-05-08 06:18:10 +0300

    Update manifest and enhance settings page functionality
    - Replaced the background page with a service worker in the manifest, improving performance and modernizing the extension architecture.
    - Added a banner iframe to the settings page, dynamically removing it if the fetch fails, enhancing user experience.
    - Updated the link in the settings page to point to the new development site, ensuring users have access to the latest resources.
    
    These changes improve the overall functionality and reliability of the extension's settings and background processes.

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,1 +1,12 @@
 import './settings/app.js';
+
+
+const bannerFrame = document.getElementById('banner-frame');
+fetch(bannerFrame.src)
+.then(response => {
+    if (!response.ok) {
+        bannerFrame.remove();
+    }
+}).catch(error => {
+    bannerFrame.remove();
+});
